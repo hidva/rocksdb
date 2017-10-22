db779d99a6dfb31c7b9439700d0f2e90925f5a0c
pp-qq <p_qp__q@163.com>
2017-10-22 13:52:19 +0800

    doc: add more doc

--- db/log_writer.cc
+++ db/log_writer.cc
@@ -30,7 +30,9 @@ Status Writer::AddRecord(const Slice& slice) {
 
   // Fragment the record if necessary and emit it.  Note that if slice
   // is empty, we still want to iterate once to emit a single
-  // zero-length record
+  // zero-length record.
+  // 注意这里是讲当 slice 为空, 也即用户传入的 slice 为空时才 emit a single zero-length record! 我最开始
+  // 以为是当 left 为 0 时也要再循环一次 emit a zero-length record 呢!
   Status s;
   do {
     const int leftover = kBlockSize - block_offset_;
@@ -45,11 +47,16 @@ Status Writer::AddRecord(const Slice& slice) {
     }
 
     // Invariant: we never leave <= kHeaderSize bytes in a block.
+    // 这个与 doc/log_format.txt 里面讲的可不一样啊, 如果按照 doc/log_format.txt 的说法这里应该是:
+    //  const int avail = kBlockSize - block_offset_;
+    // 不过虽然有一点差异, 但仍然是兼容的, 所以不影响 log reader.
+    // Q: 哪为啥要 leave kHeaderSize 呢?
     const int avail = kBlockSize - block_offset_ - kHeaderSize;
     assert(avail > 0);
 
     const size_t fragment_length = (left < avail) ? left : avail;
 
+    // 这段挺机智的.
     RecordType type;
     const bool begin = (ptr == slice.data());
     const bool end = (left == fragment_length);
@@ -66,6 +73,8 @@ Status Writer::AddRecord(const Slice& slice) {
     s = EmitPhysicalRecord(type, ptr, fragment_length);
     ptr += fragment_length;
     left -= fragment_length;
+    // Q: EmitPhysicalRecord() 失败会导致 AddRecord() 提前返回, 那么后续还会调用 AddRecord() 么? 如果
+    // 继续调用, 那么后续 AddRecord() 可能就被污染了哇!
   } while (s.ok() && left > 0);
   return s;
 }
@@ -93,6 +102,7 @@ Status Writer::EmitPhysicalRecord(RecordType t, const char* ptr, size_t n) {
       s = dest_->Flush();
     }
   }
+  // 按我理解 EmitPhysicalRecord() 在失败时应该保持原样吧, 也即不应该更新 block_offset_ 吧当失败时.
   block_offset_ += kHeaderSize + n;
   return s;
 }
